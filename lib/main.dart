71ff652b91f201464e21ee947bfbd765aefc556a
Manuel C. <manuelwn21@gmail.com>
2026-02-07 22:51:17 -0300

    chore: adds app running information log

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:ibiapabaapp/app/app.dart';
+import 'package:ibiapabaapp/core/logger/log_tags.dart';
+import 'package:ibiapabaapp/core/logger/logger.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: '.env');
   runApp(const App());
+  logger.i('${LogTags.info} app running');
 }
