e98c4866a838e16d4f471a2ec67a665719412b25
Manuel C. <manuelwn21@gmail.com>
2026-02-07 23:22:15 -0300

    chore: customizes logger to improve logging redability

---
+++ lib/core/logger/log_tags.dart
@@ -0,0 +1,20 @@
+class LogTags {
+  static const success = "[SUCCESS]";
+  static const error = "[ERROR]";
+  static const info = "[INFO]";
+  static const request = "[REQUEST]";
+  static const response = "[RESPONSE]";
+
+  // LAYERS
+  static const dioInterceptor = "[DIO_INTERCEPTOR]";
+  static const dioErrorMapper = "[DIO_ERROR_MAPPER]";
+  static const network = '[NETWORK]';
+  static const datasource = '[DATASOURCE]';
+  static const repository = '[REPOSITORY]';
+  static const controller = '[CONTROLLER]';
+
+  // FEATURES
+  static const auth = "[AUTH]";
+  static const login = "[LOGIN]";
+  static const register = "[REGISTER]";
+}
