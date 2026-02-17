9eabe50a68be12078b2fd352478a6c4bb77b3e49
Manuel C. <manuelwn21@gmail.com>
2026-02-17 11:55:37 -0300

    style(LoginSheet): adjust height with isScrollControlled

--- lib/features/auth/presentation/widgets/login/login_sheet.dart
+++ lib/features/auth/presentation/widgets/login/login_sheet.dart
@@ -4,36 +4,41 @@ import 'package:ibiapabaapp/features/auth/presentation/controllers/login_control
 import 'package:ibiapabaapp/features/auth/presentation/widgets/login/login_form.dart';
 import 'package:ibiapabaapp/features/auth/presentation/widgets/login/login_header.dart';
 
-// TODO: ajeitar altura do sheet
 void showLoginSheet({
   required LoginController controller,
   required BuildContext context,
 }) {
+  final double screenHeight = MediaQuery.of(context).size.height;
+
   showModalBottomSheet(
     context: context,
     barrierColor: Colors.black45,
-    enableDrag: false,
-    isDismissible: false,
-
-    builder: (context) => Container(
-      decoration: BoxDecoration(
-        color: context.theme.colors.background,
-        borderRadius: const BorderRadius.vertical(top: Radius.circular(24)),
-      ),
-
-      child: SingleChildScrollView(
-        padding: const EdgeInsets.all(24),
-        physics: const PageScrollPhysics(),
-
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          spacing: 16,
-
-          children: [
-            const LoginHeader(),
-            const FDivider(),
-            LoginForm(controller: controller),
-          ],
+    enableDrag: true,
+    isDismissible: true,
+    isScrollControlled: true,
+    builder: (context) => SizedBox(
+      height: screenHeight * 0.85,
+      child: Container(
+        decoration: BoxDecoration(
+          color: context.theme.colors.background,
+          borderRadius: const BorderRadius.vertical(top: Radius.circular(24)),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+          child: SingleChildScrollView(
+            physics: const ClampingScrollPhysics(),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                const LoginHeader(),
+                const SizedBox(height: 16),
+                const FDivider(),
+                const SizedBox(height: 16),
+                LoginForm(controller: controller),
+              ],
+            ),
+          ),
         ),
       ),
     ),
