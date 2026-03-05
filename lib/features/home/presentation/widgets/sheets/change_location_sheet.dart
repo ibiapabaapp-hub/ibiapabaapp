e9557ccaec14ed3943bf6feb65aff3311ea58732
Manuel C. <manuelwn21@gmail.com>
2026-03-05 15:17:25 -0300

    refactor(ChangeLocationSheet): surrounds with SafeArea + use GoRouter

--- lib/features/home/presentation/widgets/sheets/change_location_sheet.dart
+++ lib/features/home/presentation/widgets/sheets/change_location_sheet.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart'; // Import necessário
 import 'package:forui/forui.dart';
+import 'package:go_router/go_router.dart';
 import 'package:ibiapabaapp/features/auth/presentation/providers/session_provider.dart';
-import 'package:ibiapabaapp/shared/ui/sheet_drag_indicator.dart';
+import 'package:ibiapabaapp/shared/ui/layout/sheet_drag_indicator.dart';
 
 void showChangeLocationSheet({required BuildContext context}) {
   showModalBottomSheet(
@@ -15,38 +16,42 @@ void showChangeLocationSheet({required BuildContext context}) {
       builder: (context, ref, child) {
         final theme = context.theme;
 
-        return Container(
-          height: MediaQuery.of(context).size.height * 0.72,
-          decoration: BoxDecoration(
-            color: theme.colors.background,
-            borderRadius: const BorderRadius.vertical(top: Radius.circular(24)),
-          ),
+        return SafeArea(
+          child: Container(
+            height: MediaQuery.of(context).size.height * 0.72,
+            decoration: BoxDecoration(
+              color: theme.colors.background,
+              borderRadius: const BorderRadius.vertical(
+                top: Radius.circular(24),
+              ),
+            ),
 
-          child: Padding(
-            padding: const EdgeInsets.fromLTRB(16, 12, 16, 16),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                const SheetDragIndicator(),
-                const SizedBox(height: 24),
-                Text(
-                  'Alternar cidade atual',
-                  style: theme.typography.xl2.copyWith(
-                    fontWeight: FontWeight.w600,
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(16, 12, 16, 16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const SheetDragIndicator(),
+                  const SizedBox(height: 24),
+                  Text(
+                    'Alternar cidade atual',
+                    style: theme.typography.lg.copyWith(
+                      fontWeight: FontWeight.w600,
+                    ),
                   ),
-                ),
-                const Spacer(),
-                FButton(
-                  onPress: () async {
-                    Navigator.of(context).pop();
-                    await ref.read(sessionProvider.notifier).logout();
-                  },
-                  style: FButtonStyle.destructive(),
-                  prefix: Icon(Icons.logout),
-                  child: const Text('Sair da conta'),
-                ),
-                const SizedBox(height: 16),
-              ],
+                  const Spacer(),
+                  FButton(
+                    onPress: () async {
+                      context.pop();
+                      await ref.read(sessionProvider.notifier).logout();
+                    },
+                    style: FButtonStyle.destructive(),
+                    prefix: Icon(Icons.logout),
+                    child: const Text('Sair da conta'),
+                  ),
+                  const SizedBox(height: 16),
+                ],
+              ),
             ),
           ),
         );
