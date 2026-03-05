7ef398e78daec0b50c34491cf4130105b8518973
Manuel C. <manuelwn21@gmail.com>
2026-03-05 15:11:16 -0300

    refactor(NowHappeningSection): uses EventCard

--- lib/features/home/presentation/widgets/now_happening_section.dart
+++ lib/features/home/presentation/widgets/now_happening_section.dart
@@ -1,50 +1,62 @@
 import 'package:flutter/material.dart';
-import 'package:forui/forui.dart';
+import 'package:go_router/go_router.dart';
+import 'package:ibiapabaapp/features/events/domain/entities/event.dart';
+import 'package:ibiapabaapp/shared/ui/fragments/events/event_card.dart';
+import 'package:ibiapabaapp/shared/ui/layout/section_header.dart';
 
 class NowHappeningSection extends StatelessWidget {
   const NowHappeningSection({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: double.infinity,
-      child: Column(
-        mainAxisAlignment: .start,
-        crossAxisAlignment: .start,
-        spacing: 16,
-        children: [
-          Text(
-            "Acontecendo agora em Ubajara",
-            style: TextStyle(fontSize: 18, fontWeight: .w600),
-            strutStyle: StrutStyle(leading: 1.2),
-          ),
-          Column(
-            spacing: 16,
-            children: [
-              ...List<SizedBox>.generate(
-                6,
-                (index) => SizedBox(
-                  width: .infinity,
-                  child: FCard(
-                    style: (style) => style.copyWith(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(16),
-                        color: context.theme.colors.secondary,
-                      ),
-                    ),
-                    title: Text(
-                      "Evento $index",
-                      style: TextStyle(fontSize: 18),
-                    ),
-                    subtitle: Text("Lorem ipsum dolor sit amet"),
-                  ),
-                ),
-              ),
-            ],
-          ),
-          const SizedBox(height: 20),
-        ],
+    final List<Event> eventList = [
+      Event(
+        id: '1',
+        slug: 'slug',
+        name: 'name',
+        type: EventType.simple,
+        reachLevel: EventReachLevel.local,
+        createdAt: DateTime.now(),
+        updatedAt: DateTime.now(),
       ),
+      Event(
+        id: '1',
+        slug: 'slug',
+        name: 'name',
+        type: EventType.simple,
+        reachLevel: EventReachLevel.local,
+        createdAt: DateTime.now(),
+        updatedAt: DateTime.now(),
+      ),
+    ];
+
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        SectionHeader(
+          title: 'Acontecendo agora',
+          onSeeAllTap: () => context.push('/app/events'),
+        ),
+        const SizedBox(height: 16),
+
+        SizedBox(
+          height: 150,
+          child: ListView.separated(
+            padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
+            scrollDirection: Axis.horizontal,
+            physics: const BouncingScrollPhysics(),
+            itemCount: eventList.length,
+            separatorBuilder: (context, index) => const SizedBox(width: 12),
+            itemBuilder: (context, index) {
+              return SizedBox(
+                width: 450,
+                child: EventCard(event: eventList.elementAt(index)),
+              );
+            },
+          ),
+        ),
+      ],
     );
   }
 }
