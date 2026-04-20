a08430263ac1bd9a5be48161c1cf8757b952a6a9
Manuel C. <manuelwn21@gmail.com>
2026-04-20 12:53:51 -0300

    refactor: update entity type from company to business in getEntityIcon function

--- lib/shared/utils/get_entity_icon.dart
+++ lib/shared/utils/get_entity_icon.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:ibiapabaapp/features/medias/domain/entity/media.dart';
+import 'package:ibiapabaapp/core/entities/entity_type.dart';
 
 IconData getEntityIcon(EntityType type) {
   switch (type) {
     case EntityType.city:
       return Icons.pin_drop_rounded;
-    case EntityType.company:
+    case EntityType.business:
       return Icons.store_rounded;
     case EntityType.event:
       return Icons.event_rounded;
